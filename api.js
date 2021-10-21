0ad6a5d6d5737ea18418d93b654f7c6ec0ed9a9a
DeviVara-Prasad <nuludevivaraprasad@gmail.com>
2021-10-21 22:59:42 +0530

    added a scrollable text

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 for(var j=1;j<=8;j++){
     for(var i=1;i<=8;i++){
     var black = document.createElement('div');
-    black.id = 'black';
+    black.className = 'black';
     var white = document.createElement('div');
-    white.id = 'white';
+    white.className = 'white';
     if((j%2==1 && i%2==1)||(j%2==0 && i%2==0)){
         document.getElementById("chess").appendChild(white);
     }
